38a9b8cb973eff975a4c64b177e9d4fd60fef6c9
Anthony Castillo <alanthonyc@gmail.com>
2015-12-18 20:12:24 -0800

    Logout - call Udacity logout api
        ## Logout
        * DELETE session

--- On the Map/OTMTabBarController.swift
+++ On the Map/OTMTabBarController.swift
@@ -27,7 +27,7 @@ class OTMTabBarController: UITabBarController {
     
     @IBAction func dismiss(sender: UIButton)
     {
-        self.self.dismissViewControllerAnimated(true, completion: nil)
+        self.logoutAndDismiss()
     }
     
     @IBAction func showAddPinView(sender: UIButton)
@@ -45,5 +45,33 @@ class OTMTabBarController: UITabBarController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func logoutAndDismiss()
+    {
+        self.logout()
+        self.self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    func logout()
+    {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "DELETE"
+        var xsrfCookie: NSHTTPCookie? = nil
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        for cookie in sharedCookieStorage.cookies as [NSHTTPCookie]! {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5)) /* subset response data! */
+            print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
+    }
 }
