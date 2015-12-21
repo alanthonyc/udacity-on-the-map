1e39c9c65eb7a379878d4ddd17ad6330eecebfa4
Anthony Castillo <alanthonyc@gmail.com>
2015-12-20 20:40:12 -0800

    Table View
        ## Table View
        * display student list in table view tab
        * reverse chronological (`updatedAt`) order
        * launch safari on cell selection

---
+++ On the Map/OTMTableViewController.swift
@@ -0,0 +1,93 @@
+//
+//  OTMTableViewController.swift
+//  On the Map
+//
+//  Created by A. Anthony Castillo on 12/20/15.
+//  Copyright © 2015 Alon Consulting. All rights reserved.
+//
+
+import UIKit
+
+class OTMTableViewController: UITableViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return Student.List.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("studentCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "\(Student.List[indexPath.row].firstName) \(Student.List[indexPath.row].lastName)"
+        cell.detailTextLabel?.text = "\(Student.List[indexPath.row].mediaURL)"
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let student = Student.List[indexPath.row]
+        UIApplication.sharedApplication().openURL(NSURL(string: "\(student.mediaURL)")!)
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
